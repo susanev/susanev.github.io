b684127222ba5ae1e5db36a5867506d7db97345a
susan <susan.ra.evans@gmail.com>
2016-10-05 12:13:44 -0700

    updated toggle nav

--- js/basic.js
+++ js/basic.js
@@ -1,10 +1,7 @@
 var displayNav = false;
 
-function showNav() {
-    var line1 = document.getElementById("line1");
-    var line2 = document.getElementById("line2");
-    var line3 = document.getElementById("line3");
-    
+/* toggles the hamburger menu when clicked */
+function toggleNav() {   
     if(displayNav){
         $('#mainNav').hide();
         $('#line1').attr('class', 'no-rotate');
@@ -18,10 +15,12 @@ function showNav() {
         $('#line2').attr('class', 'line2-rotate');
         $('#line3').attr('class', 'line3-rotate');
         displayNav = true;
-
     }
 }
 
+/*  if window resized to bigger than 973
+    remove hamburger; else show hamburger
+*/
 $(window).resize(function() {
     if($(window).width() > 973){
         $('#mainNav').show();
@@ -34,6 +33,11 @@ $(window).resize(function() {
     }
 });
 
+window.onload = function() {
+    // stuff to happen after load
+}
+
+// google analytics code
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -41,4 +45,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-84803201-1', 'auto');
 ga('send', 'pageview');
-
