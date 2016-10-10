d05c2860236e745e75482b83830fab7858ef1e7e
susan <susan.ra.evans@gmail.com>
2016-10-10 11:41:30 -0700

    reducded redudancy for toggleNav

--- js/basic.js
+++ js/basic.js
@@ -1,16 +1,16 @@
 var displayNav = false;
 
 /* toggles the hamburger menu when clicked */
-function toggleNav() {   
+function toggleNav() { 
+    $('#mainNav').toggle();
+    
     if(displayNav){
-        $('#mainNav').hide();
         $('#line1').attr('class', 'no-rotate');
         $('#line2').attr('class', 'no-rotate');
         $('#line3').attr('class', 'no-rotate');
         displayNav = false;
     }
     else {
-       $('#mainNav').show();
         $('#line1').attr('class', 'line1-rotate');
         $('#line2').attr('class', 'line2-rotate');
         $('#line3').attr('class', 'line3-rotate');
