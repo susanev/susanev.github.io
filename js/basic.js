5d72db76f9df43a2d3b613905788dacb9c238459
susan <susan.ra.evans@gmail.com>
2016-11-11 19:13:10 -0800

    fixe js errors

--- js/basic.js
+++ js/basic.js
@@ -33,31 +33,9 @@ $(window).resize(function() {
     }
 });
 
-$(function() {
-    $(".rslides").responsiveSlides({
-        auto: false,             // Boolean: Animate automatically, true or false
-        speed: 500,            // Integer: Speed of the transition, in milliseconds
-        timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-        pager: true,           // Boolean: Show pager, true or false
-        nav: false,             // Boolean: Show navigation, true or false
-        random: false,          // Boolean: Randomize the order of the slides, true or false
-        pause: false,           // Boolean: Pause on hover, true or false
-        pauseControls: true,    // Boolean: Pause when hovering controls, true or false
-        prevText: "Previous",   // String: Text for the "previous" button
-        nextText: "Next",       // String: Text for the "next" button
-        maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
-        navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
-        manualControls: "",     // Selector: Declare custom pager navigation
-        namespace: "rslides",   // String: Change the default namespace used
-        before: function(){},   // Function: Before callback
-        after: function(){}     // Function: After callback
-    });
-});
-
-
 window.onload = function() {
     // stuff to happen after load
-}
+};
 
 // google analytics code
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
