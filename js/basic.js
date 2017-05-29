e90b87e62187db8c5409033e5d82d4419c6fe97c
susanev <susan.ra.evans@gmail.com>
2017-05-29 11:00:38 -0700

    fixed contact, thank you, and a few small things on index, resume. removed about page, as not needed

--- js/basic.js
+++ js/basic.js
@@ -49,6 +49,58 @@ window.onload = function() {
     // stuff to happen after load
 };
 
+/*  when submit button clicked
+    check that every field is filled in
+    and that email contains @
+*/
+function checkValues() {
+    return checkAllValues();
+}
+
+function checkValue(element) {
+    if($(element).hasClass("error-border") && $(element).val() != ""){
+        $(element).removeClass("error-border");
+        $(element).addClass("reset-border");
+        checkAllValues();
+    }
+
+    else if(!$(element).hasClass("error-border") && $(element).val() == ""){
+        $(element).addClass("error-border");
+        $(element).removeClass("reset-border");
+    }
+}
+
+function checkAllValues() {
+    var elements = $(".form-field");
+
+    var empty = 0;
+    for (var i=0; i<elements.length; i++) {
+        if($(elements[i]).val() == ""){
+            $(elements[i]).addClass("error-border");
+            empty++;
+        }
+        else {
+            $(elements[i]).removeClass("error-border");
+            $(elements[i]).addClass("reset-border");
+        }
+    }
+        
+    if(empty == 0){
+        $("#user-msg").hide();
+        return true;
+    }
+
+    else {
+        $("#user-msg").text("Please fill in all of the fields");;
+        $("#user-msg").show();
+        return false;
+    }
+}
+
+window.onload = function() {
+    $("#user-msg").hide();
+}
+
 // google analytics code
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
